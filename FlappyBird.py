e974292443c75be27d6fbd9fc26cae430b848af3
Rômulo Souza Fernandes <rromulofer@gmail.com>
2023-02-02 02:39:48 -0300

    Update FlappyBird.py

--- FlappyBird.py
+++ FlappyBird.py
@@ -5,13 +5,19 @@ import random
 TELA_LARGURA = 500
 TELA_ALTURA = 800
 
-IMAGEM_CANO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','pipe.png')))
-IMAGEM_CHAO = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','base.png')))
-IMAGEM_BACKGROUND = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bg.png')))
-IMAGENS_PASSARO = [ # lista de imagens
-    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bird1.png'))), 
-    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bird2.png'))),
-    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs','bird3.png'))),
+IMAGEM_CANO = pygame.transform.scale2x(
+    pygame.image.load(os.path.join('imgs', 'pipe.png')))
+IMAGEM_CHAO = pygame.transform.scale2x(
+    pygame.image.load(os.path.join('imgs', 'base.png')))
+IMAGEM_BACKGROUND = pygame.transform.scale2x(
+    pygame.image.load(os.path.join('imgs', 'bg.png')))
+IMAGENS_PASSARO = [  # lista de imagens
+    pygame.transform.scale2x(pygame.image.load(
+        os.path.join('imgs', 'bird1.png'))),
+    pygame.transform.scale2x(pygame.image.load(
+        os.path.join('imgs', 'bird2.png'))),
+    pygame.transform.scale2x(pygame.image.load(
+        os.path.join('imgs', 'bird3.png'))),
 ]
 
 pygame.font.init()
@@ -24,28 +30,66 @@ class Passaro:
     ROTACAO_MAXIMA = 25
     VELOCIDADE_ROTACAO = 20
     TEMPO_ANIMACAO = 5
-    
+
     # atributos do pássaro
-    def __init__(self, x, y)
+    def __init__(self, x, y):
         self.x = x
         self.y = y
         self.angulo = 0
         self.velocidade = 0
         self.altura = self.y
         self.tempo = 0
-        self.contagem_iamgem = 0
-        self.imagem = IMGS[0] # primeiro item da lista de imagens
+        self.contagem_imagem = 0
+        self.imagem = self.IMGS[0]  # primeiro item da lista de imagens
 
     def pular(self):
         self.velocidade = -10.5
         self.tempo = 0
         self.altura = self.y
 
+    def mover(self):
+        # calcular o deslocamento
+        self.tempo += 1
+        deslocamento = 1.5 * (self.tempo**2) + self.velocidade * self.tempo
 
+        # restringir o deslocamento
+        if deslocamento > 16:
+            deslocamento = 16
+        elif deslocamento < 0: # boost no pulo, para não cair mais que pula
+            deslocamento -= 2 
+         
+        self.y += deslocamento
 
-class Cano: 
+        # ângulo do pássaro
+        if deslocamento < 0 or self.y < (self.altura + 50):
+            if self.angulo < self.ROTACAO_MAXIMA:
+                self.angulo = self.ROTACAO_MAXIMA
+        else:
+            if self.angulo > -90:
+                self.angulo -= self.VELOCIDADE_ROTACAO
+
+    def desenhar(self):
+        # definir qual imagem do pássaro vai usar
+        self.contagem_imagem += 1
+
+        if self.contagem_imagem < self.TEMPO_ANIMACAO:
+            self.imagem = self.IMGS[0]
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO*2:
+            self.imagem = self.IMGS[1]
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO*3:
+            self.imagem = self.IMGS[2]
+        elif self.contagem_imagem < self.TEMPO_ANIMACAO*4:
+            self.imagem = self.IMGS[1]
+        elif self.contagem_imagem >= self.TEMPO_ANIMACAO*4 + 1:
+            self.imagem = self.IMGS[0]
+            self.contagem_imagem = 0
+
+        # se o pássaro estiver caindo a asa não irá bater
+
+        # desenhar a imagem
+
+class Cano:
     pass
-
 
 
 class Chao:
